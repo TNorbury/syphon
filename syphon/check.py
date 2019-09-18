ef82c4c8608f418a7a5e76b4f9437bde9e0bd142
Evan Hall <evan.hall@keithley.com>
2019-09-18 12:34:20 -0400

    Updated check imports and param names

--- syphon/check.py
+++ syphon/check.py
@@ -14,18 +14,18 @@ DEFAULT_FILE = ".sha256sums"
 
 def check(
     cache_filepath: str,
-    checksum_filepath: Optional[str] = None,
-    checksum_line_split: Optional[Callable[[str], Optional[SplitResult]]] = None,
+    hash_filepath: Optional[str] = None,
+    hash_line_split: Optional[Callable[[str], Optional[SplitResult]]] = None,
     verbose: bool = False,
 ) -> bool:
     """Verify the integrity of the built cache file.
 
     Args:
         cache_filepath: Absolute path to the target output file.
-        checksum_filepath: Absolute path to a file containing a SHA256 sum of the
+        hash_filepath: Absolute path to a file containing a SHA256 sum of the
             cache. If not given, then the default is calculated by joining the cache
             directory with `syphon.check.DEFAULT_FILE`.
-        checksum_line_split: A callable object that returns a `syphon.util.SplitResult`
+        hash_line_split: A callable object that returns a `syphon.util.SplitResult`
             from a given line or None if the line is in an unexpected format. Returning
             None raises a MalformedLineError.
         verbose: Whether to print what is being done to the standard output.
@@ -35,44 +35,52 @@ def check(
     """
     import os
 
-    from . import errors, util
+    from .errors import MalformedLineError
+    from .util import HashEntry, HashFile
 
-    if checksum_filepath is None:
+    if hash_filepath is None:
         cachepath, cachefile = os.path.split(cache_filepath)
-        checksum_filepath = os.path.join(cachepath, DEFAULT_FILE)
+        hash_filepath = os.path.join(cachepath, DEFAULT_FILE)
 
-    if not os.path.exists(checksum_filepath):
+    if not os.path.exists(hash_filepath):
         if verbose:
-            print("No file exists @ {}".format(checksum_filepath))
+            print("No file exists @ {}".format(hash_filepath))
         return False
 
-    expected_entry: Optional[util.HashEntry] = None
-    actual_entry = util.HashEntry(cache_filepath)
+    expected_entry: Optional[HashEntry] = None
+    actual_entry = HashEntry(cache_filepath)
 
     try:
-        with util.HashFile(checksum_filepath) as hf:
-            for next_entry in hf.entries(checksum_line_split):
-                if next_entry.filepath == actual_entry.filepath:
-                    expected_entry = next_entry
+        # Find the hash entry for the provided cache filepath.
+        with HashFile(hash_filepath) as hashfile:
+            hashfile.line_split = hash_line_split
+            for entry in hashfile:
+                if entry.filepath == actual_entry.filepath:
+                    expected_entry = entry
                     break
     except OSError:
         if verbose:
-            print("Error reading hash file @ {}".format(checksum_filepath))
+            print("Error reading hash file @ {}".format(hash_filepath))
         return False
-    except errors.MalformedLineError as err:
+    except MalformedLineError as err:
         if verbose:
             print('Error parsing hash entry "{}"'.format(err.line))
         return False
     finally:
-        del hf
+        del hashfile
 
     if expected_entry is None:
         if verbose:
             print(
                 'No entry for file "{0}" found in "{1}"'.format(
-                    cache_filepath, checksum_filepath
+                    cache_filepath, hash_filepath
                 )
             )
         return False
 
-    return expected_entry.hash == actual_entry.hash
+    try:
+        return expected_entry.hash == actual_entry.hash
+    except OSError:
+        if verbose:
+            print("Error reading cache file @ {}".format(cache_filepath))
+        return False
