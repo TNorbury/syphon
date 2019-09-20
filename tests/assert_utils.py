491b8feb509da06025b7b8f8bee827c3308044fb
Evan Hall <evan.hall@keithley.com>
2019-09-20 18:08:31 -0400

    Added a capsys matcher helper

--- tests/assert_utils.py
+++ tests/assert_utils.py
@@ -4,8 +4,9 @@
    Licensed under MIT (https://github.com/tektronix/syphon/blob/master/LICENSE)
 
 """
-from _pytest.capture import CaptureFixture, CaptureResult
+from typing import List
 
+from _pytest.capture import CaptureResult
 
 
 def assert_captured_outerr(captured: CaptureResult, has_stdout: bool, has_stderr: bool):
@@ -21,3 +22,19 @@ def assert_captured_outerr(captured: CaptureResult, has_stdout: bool, has_stderr
         assert len(captured.err) > 0, no_data_msg.format("stderr")
     else:
         assert len(captured.err) == 0, data_msg.format("stderr", captured.err)
+
+
+def assert_matches_outerr(
+    captured: CaptureResult, match_out: List[str], match_err: List[str]
+):
+    match_msg = 'Cannot find "{0}" in "{1}" from {2}'
+
+    for out in match_out:
+        assert str(captured.out).find(out) != -1, match_msg.format(
+            out, captured.out, "stdout"
+        )
+
+    for err in match_err:
+        assert str(captured.err).find(err) != -1, match_msg.format(
+            err, captured.err, "stderr"
+        )
